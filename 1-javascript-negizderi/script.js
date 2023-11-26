6ec7e8227dc940a47f0db0df1874b812e66b4da4
Salavat <salavat.islamov.96@gmail.com>
2023-11-26 11:36:28 +0100

    Ozgormolor

---
+++ 1-javascript-negizderi/script.js
@@ -0,0 +1,49 @@
+// Маани деген эмне?
+console.log("Mirbek");
+console.log(18);
+
+// Озгормо деген эмне?
+let firstName = "Mirbek";
+console.log(firstName);
+//let firstName = "Nurbek"; мындай инструкция ката!
+firstName = "Nurbek";
+console.log(firstName);
+
+let age = 18;
+age = 19;
+console.log(age);
+
+// Озгормону кантип жарялайбыз?
+/*
+let secondName;
+secondName = "Asanov";
+console.log(secondName);
+*/
+
+let secondName = "Asanov",
+  nationality = "kyrgyz";
+console.log(secondName, nationality);
+
+// Озгормонун аталышындагы чектоолор
+//let 1job = "Chach tarach";
+// let menin-birinchi-mugalimim = "Дуйшон";
+//let function;
+//let return;
+let job1 = "Chach tarach";
+let $myProffession = "Programmer";
+let _test = "Test";
+let menin_birinchi_mugalimim = "Дуйшон";
+// Озгормоону туура атоо (camel case)
+let meninBirichiMugalimim = "Гапыр агай";
+let a = "Гапыр агай";
+
+let myFirstJob = "barber";
+
+var myVariable = "Test";
+console.log(myVariable);
+let profession;
+console.log(profession);
+
+// Контстанта деген эмне
+const myBirthDay = "01.01.2000";
+console.log(myBirthDay);
